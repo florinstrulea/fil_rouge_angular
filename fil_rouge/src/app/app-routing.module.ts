3de97e6be0d62580abc36f7f2e0ea53222b3d404
Florin Strulea <florin_strulea@yahoo.com>
2022-11-02 11:43:35 +0100

    résolution des composants security

--- fil_rouge/src/app/app-routing.module.ts
+++ fil_rouge/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/security/login/login.component';
+import { RegisterComponent } from './components/security/register/register.component';
+import { ResetPasswordComponent } from './components/security/reset-password/reset-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'forgot-password',
+    component: ResetPasswordComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- fil_rouge/src/app/components/security/reset-password/reset-password.component.spec.ts
+++ fil_rouge/src/app/components/security/reset-password/reset-password.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ResetFormComponent } from './reset-form.component';
+import { ResetPasswordComponent } from './reset-password.component';
 
-describe('ResetFormComponent', () => {
-  let component: ResetFormComponent;
-  let fixture: ComponentFixture<ResetFormComponent>;
+describe('ResetPasswordComponent', () => {
+  let component: ResetPasswordComponent;
+  let fixture: ComponentFixture<ResetPasswordComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ResetFormComponent ]
+      declarations: [ ResetPasswordComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ResetFormComponent);
+    fixture = TestBed.createComponent(ResetPasswordComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
