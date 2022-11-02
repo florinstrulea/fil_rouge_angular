624d39e5bf613609de682da3a16077099a74d4f7
Florin Strulea <coolfloboy@gmail.com>
2022-11-02 02:33:32 +0100

    image sur div de gauche

--- fil_rouge/src/app/components/security/utils/image-left/image-left.component.ts
+++ fil_rouge/src/app/components/security/utils/image-left/image-left.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-image-left',
@@ -11,5 +11,7 @@ export class ImageLeftComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @Input() imageUrl = '';
 
 }
