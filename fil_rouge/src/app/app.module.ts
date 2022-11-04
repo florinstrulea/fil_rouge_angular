e2128e060c04d2ef7a897ab0830b8cbe17882583
RachidSoukane <74837390+RachidSoukane@users.noreply.github.com>
2022-11-04 10:44:16 +0100

    [RS] ajout du Navbar component

--- fil_rouge/src/app/app.module.ts
+++ fil_rouge/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthComponent } from './auth/auth.component';
 import { NotFoundComponent } from './components/others/not-found/not-found.component';
+import { NavbarComponent } from './components/commons/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    NavbarComponent
 
   ],
   imports: [
