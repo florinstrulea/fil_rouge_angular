032d30ba5620f71f19f636da3f3b4cb99d8de343
Florin Strulea <florin_strulea@yahoo.com>
2022-11-04 16:29:42 +0100

    modification de composant login et ses fichiers

--- fil_rouge/src/app/components/security/login/login.component.ts
+++ fil_rouge/src/app/components/security/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserLogin } from 'src/app/interfaces/user-login';
+import { Auth } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  user = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
+  constructor(private authService: Auth, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    this.authService.login(this.user.value as UserLogin).subscribe((luser) => {
+      if (this.authService.isUser(luser)) {
+        //this.router.navigateByUrl('/auth/register');
+        sessionStorage.setItem('connected', 'true');
+        sessionStorage.setItem('user', JSON.stringify(luser));
+        console.log('connected');
+      } else console.log('not-connected');
+    });
   }
-
 }
--- fil_rouge/src/app/services/auth/auth.service.ts
+++ fil_rouge/src/app/services/auth/auth.service.ts
@@ -1,16 +1,40 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { User } from 'src/app/interfaces/user';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Status } from 'src/app/interfaces/status';
+import { User } from 'src/app/interfaces/user';
+import { UserLogged } from 'src/app/interfaces/user-logged';
+import { UserLogin } from 'src/app/interfaces/user-login';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
   userUrl: string = 'http://localhost:8080/register';
+  loginUrl: string = 'http://localhost:8080/login';
+  // private curUser = new BehaviorSubject<AuthStatus>({
+  //   connected: false,
+  // });
+
+  // curUserObservable = this.curUser.asObservable();
+
   constructor(private http: HttpClient) {}
 
   register(user: User): Observable<User> {
     return this.http.post<User>(this.userUrl, user);
   }
+
+  login(user: UserLogin): Observable<UserLogged> {
+    return this.http.post<UserLogged>(this.loginUrl, user);
+  }
+
+  isUser(obj: any): obj is User {
+    return 'username' in obj;
+  }
+  isStatus(obj: any): obj is Status {
+    return 'response' in obj;
+  }
+  // isConnected(): boolean {
+  //   return this.curUser.value.connected;
+  // }
 }
