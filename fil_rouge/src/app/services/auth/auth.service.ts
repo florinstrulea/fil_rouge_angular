713477aa7e7b9d8e3479397749a129d4de320099
RachidSoukane <74837390+RachidSoukane@users.noreply.github.com>
2022-11-05 13:32:09 +0100

    [RS] Rebase - authService ok

--- fil_rouge/src/app/services/auth/auth.service.ts
+++ fil_rouge/src/app/services/auth/auth.service.ts
@@ -1,40 +1,60 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Status } from 'src/app/interfaces/status';
 import { User } from 'src/app/interfaces/user';
-import { UserLogged } from 'src/app/interfaces/user-logged';
-import { UserLogin } from 'src/app/interfaces/user-login';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { AuthStatus } from 'src/app/interfaces/auth-status';
+import { Status } from 'src/app/interfaces/status';
+import { environment } from 'src/environments/environment';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
-  userUrl: string = 'http://localhost:8080/register';
-  loginUrl: string = 'http://localhost:8080/login';
+  // userUrl: string = 'http://localhost:8080/register';
+  // loginUrl: string = 'http://localhost:8080/login';
   // private curUser = new BehaviorSubject<AuthStatus>({
   //   connected: false,
   // });
 
   // curUserObservable = this.curUser.asObservable();
+  private curUser = new BehaviorSubject<AuthStatus>({
+    connected : false
+  });
+
+  curUserObservable = this.curUser.asObservable();
+  userUrl: string = environment.apiUrl+"register";
+  loginUrl : string = environment.apiUrl+"login";
+  logoutUrl : string = environment.apiUrl+"logout";
 
   constructor(private http: HttpClient) {}
 
-  register(user: User): Observable<User> {
-    return this.http.post<User>(this.userUrl, user);
+  register(user: User): Observable<User | Status> {
+    return this.http.post<User | Status >(this.userUrl, user);
+  }
+  login(user : User) : Observable<User | Status>{
+    return this.http.post<User | Status>(this.loginUrl,user, {withCredentials : true});
+  }
+  getUser() : Observable<User | Status>{
+  return this.http.get< User | Status>(this.loginUrl, {withCredentials : true});
+  }
+  logout() : Observable<Status>{
+    return this.http.get<Status>(this.logoutUrl, {withCredentials : true});
+  } 
+  setAuthStatus(auth : AuthStatus){
+    this.curUser.next(auth);
+  }
+  getCurrentUser() : AuthStatus{
+    return this.curUser.value;
+  }
+  isUser(obj : any): obj is User{
+    return "userName" in obj;
+  }
+  isStatus(obj : any) : obj is Status{
+    return "response" in obj;
+  }
+  isConnected() : boolean {
+    return this.curUser.value.connected;
   }
 
-  login(user: FormData): Observable<any> {
-    return this.http.post<any>(this.loginUrl, user);
-  }
 
-  isUser(obj: any): obj is User {
-    return 'username' in obj;
-  }
-  isStatus(obj: any): obj is Status {
-    return 'response' in obj;
-  }
-  // isConnected(): boolean {
-  //   return this.curUser.value.connected;
-  // }
 }
