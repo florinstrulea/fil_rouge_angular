6ff260a80f366900f87b9b9c87fc6d09207c83ae
Florin Strulea <coolfloboy@gmail.com>
2022-11-07 15:29:08 +0100

    ajout du composant choose player

--- fil_rouge/src/app/components/game/choose-player/choose-player.component.ts
+++ fil_rouge/src/app/components/game/choose-player/choose-player.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./choose-player.component.css']
 })
 export class ChoosePlayerComponent implements OnInit {
+  warriorFemale: string = "../../../../assets/choose-player/Warrior_F_equiped.png"
+  warriorMale: string = "../../../../assets/choose-player/Warrior_Male_equiped.png"
+  archerFemale: string = "../../../../assets/choose-player/Archer_Female.png"
+  archerMale: string = "../../../../assets/choose-player/Archer_Male.png"
 
   constructor() { }
 
