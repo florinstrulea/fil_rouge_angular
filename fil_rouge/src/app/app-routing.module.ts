dd7f7abd976b4c3e1c7c23f948aaf5033ad03013
Florin Strulea <coolfloboy@gmail.com>
2022-11-07 11:17:43 +0100

    resolution import auth module

--- fil_rouge/src/app/app-routing.module.ts
+++ fil_rouge/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './modules/auth/auth.component';
+
 import { NotFoundComponent } from './components/others/not-found/not-found.component';
-import { AuthenticationGuard } from './guards/auth/authentication.guard';
+
 import { HomeComponent } from './components/game/home/home.component';
+import { AuthComponent } from './modules/auth/auth.component';
 
 const routes: Routes = [
   {
