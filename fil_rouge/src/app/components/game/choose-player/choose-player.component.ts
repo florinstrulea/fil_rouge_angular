31884d8eebe8c376d11059f8bafb3c8ee354aff7
Florin Strulea <coolfloboy@gmail.com>
2022-11-08 14:13:52 +0100

    ajout des liens pour les images choose player et envoye de l'image selectionné dans main-page

--- fil_rouge/src/app/components/game/choose-player/choose-player.component.ts
+++ fil_rouge/src/app/components/game/choose-player/choose-player.component.ts
@@ -7,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChoosePlayerComponent implements OnInit {
   warriorFemale: string = "../../../../assets/choose-player/Warrior_F_equiped.png"
-  warriorMale: string = "../../../../assets/choose-player/Warrior_Male_equiped.png"
+  warriorMale: string = "../../../../assets/choose-player/Warrior_Male_equiped.png/Warrior_Male_equiped.png"
   archerFemale: string = "../../../../assets/choose-player/Archer_Female.png"
   archerMale: string = "../../../../assets/choose-player/Archer_Male.png"
   heroId?: string;
+  listPhotos = [
+    {
+      id: 1,
+      link: "/Warrior_F_equiped.png",
+    },
+    {
+      id: 2,
+      link: "/Warrior_Male_equiped.png",
+    },
+    {
+      id: 7,
+      link: "/Archer_Female.png",
+    },
+    {
+      id: 8,
+      link: "/Archer_Male.png",
+    },
+
+
+  ];
   constructor() { }
 
   ngOnInit(): void {
