9e90dd8921a9204243a043f997820f07f77feac7
Florin Strulea <florin_strulea@yahoo.com>
2022-11-14 09:19:58 +0100

    monor update

--- fil_rouge/src/app/components/security/login/login.component.ts
+++ fil_rouge/src/app/components/security/login/login.component.ts
@@ -18,33 +18,20 @@ export class LoginComponent implements OnInit {
   errorMessage = "L'email / username ou mot de passe n'est pas valid";
   invalidLogin = false;
 
-
   login = new FormGroup({
     username: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required]),
   });
 
-  constructor(private authService: Auth, private router: Router, http: HttpClient) { }
+  constructor(
+    private authService: Auth,
+    private router: Router,
+    http: HttpClient
+  ) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   onSubmit() {
-<<<<<<< HEAD
-    let formData = new FormData();
-    formData.append('username', this.user.get('username')!.value!);
-    formData.append('password', this.user.get('password')!.value!);
-    console.log(formData);
-
-    this.authService.login(formData).subscribe((luser: FormData) => {
-      console.log(luser);
-      // if (this.authService.isUser(luser)) {
-      //   //this.router.navigateByUrl('/auth/register');
-      //   sessionStorage.setItem('connected', 'true');
-      //   sessionStorage.setItem('user', JSON.stringify(luser));
-      //   console.log('connected');
-      // } else console.log('not-connected');
-    });
-=======
     if (!sessionStorage.getItem('token')) {
       let user: UserLogin = {} as UserLogin;
       user.password = this.login.controls.password.value!;
@@ -59,22 +46,18 @@ export class LoginComponent implements OnInit {
           sessionStorage.setItem('user', JSON.stringify(this.storedUser));
           this.authService.setAuthStatus({
             connected: true,
-            user: JSON.parse(sessionStorage.getItem("user")!)
+            user: JSON.parse(sessionStorage.getItem('user')!),
           });
-          this.router.navigateByUrl("/game/home");
+          this.router.navigateByUrl('/game/home');
         } else {
           this.invalidLogin = true;
-          this.errorMessage = "L'email / username ou mot de passe n'est pas valid"
-
+          this.errorMessage =
+            "L'email / username ou mot de passe n'est pas valid";
         }
       });
     } else {
       this.login.reset();
-
     }
-
-
->>>>>>> 164d7f5fcbd6e9e68937ec691ff56770425a1140
   }
 
   emptyFields() {
@@ -85,8 +68,5 @@ export class LoginComponent implements OnInit {
       this.errorMessage = '';
       this.invalidLogin = false;
     }
-
   }
-
-
 }
