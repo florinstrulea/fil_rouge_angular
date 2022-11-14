87d94f8c873d3e48448d7b75b72a55c2e2493ec8
Florin Strulea <florin_strulea@yahoo.com>
2022-11-14 16:57:26 +0100

    modif de inventory card

--- fil_rouge/src/app/guards/arena/arena.guard.ts
+++ fil_rouge/src/app/guards/arena/arena.guard.ts
@@ -1,24 +1,33 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ArenaGuard implements CanActivate {
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    let playerChosen = sessionStorage.getItem('playerChosen');
 
-    let playerChosen = sessionStorage.getItem("playerChosen");
-
-    if (playerChosen == 'false') {
-      return this.router.parseUrl('/game/choose-player')
+    if (playerChosen == 'false' || playerChosen == null) {
+      return this.router.parseUrl('/game/choose-player');
     }
 
     return true;
   }
-
 }
--- fil_rouge/src/app/guards/main-page/main-page.guard.ts
+++ fil_rouge/src/app/guards/main-page/main-page.guard.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MainPageGuard implements CanActivate {
-  constructor(private router: Router) {
-
-  }
+  constructor(private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    let playerChosen = sessionStorage.getItem('playerChosen');
 
-    let playerChosen = sessionStorage.getItem("playerChosen");
-
-    if (playerChosen == 'false') {
-      return this.router.parseUrl('/game/choose-player')
+    if (playerChosen == 'false' || playerChosen == null) {
+      return this.router.parseUrl('/game/choose-player');
     }
     return true;
   }
-
-
-
 }
